2406b02413ad4d00de32dc548dd34c836934fbcd
Matteo Rosati <rosati.matteo@gmail.com>
2013-12-08 13:58:20 +0100

    Ignore database configuration

--- app/config/database.dist.php
+++ app/config/database.dist.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'roombooking',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
