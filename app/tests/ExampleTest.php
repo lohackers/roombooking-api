99614a67d171809a939759c7713bc17e7cc63cc8
Matteo Rosati <rosati.matteo@gmail.com>
2013-12-08 20:35:58 +0000

    Add initial tests

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -10,7 +10,12 @@ class ExampleTest extends TestCase {
 	public function testBasicExample()
 	{
 		$crawler = $this->client->request('GET', '/');
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
 
+	public function testGetBookings()
+	{
+		$crawler = $this->client->request('GET', '/bookings');
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
 
