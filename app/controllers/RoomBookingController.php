18d56ab064fd95a40fa5bcf30cadc83b16d708c9
Matteo Rosati <mrosati@h-art.it>
2013-12-12 14:19:12 +0100

    Add 'upcoming' and 'onair' GET parameters to RoomBookingController@index

--- app/controllers/RoomBookingController.php
+++ app/controllers/RoomBookingController.php
@@ -9,6 +9,29 @@ class RoomBookingController extends \BaseController {
 	 */
 	public function index($id)
 	{
+		if ( Input::get('upcoming') )
+		{
+			return Booking::with('user')
+				->where('room_id', '=', $id)
+				->where('from', '>=', DB::raw('NOW()'))
+				->where('from', '<=', DB::raw('NOW() + INTERVAL 15 MINUTE'))
+				->get();
+		}
+
+		if ( Input::get('onair') )
+		{
+			$q = Booking::with('user')
+				->where('room_id', '=', $id)
+				->where(DB::raw('NOW()'), '>=', DB::raw('`from`'))
+				->where(DB::raw('NOW()'), '<=', DB::raw('`to`'))
+				->get();
+
+			$queries = DB::getQueryLog();
+			$last_query = end($queries);
+
+			return $q;
+		}
+
 		return Booking::with('user')
 			->where('room_id', '=', $id)
 			->get();
